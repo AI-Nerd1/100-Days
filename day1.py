5d20f327cbdd4a56dccad79bc930d58cb88d11b3
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-09-04 13:28:52 +0300

    Add files via upload

--- day1.py
+++ day1.py
@@ -1,29 +1,34 @@
-print("Day 1 - Python Print Function")
-print("The function is declared like this:")
-print("print('what to print')")
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
 
-print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
-
-
-print("       ====================================\n                  Debugging")
-print("                   Day - 1\n             String manipulation")
-print("String Concatenation is done with the +" + " sign")
-print('e.g. print("Hello " + "world")')
-print("New lines can be created with backslash and the letter n.\n")
-print("Trying a new change to the init commit, on day 1")
-print("Working on staged changes.")
+# print("Day 1 - Python Print Function\nThe function is declared like this:\nprint('what to print')")
 
 
-input("What is your name?  ")
+# print("       ====================================\n                  Debugging")
+# print("                   Day - 1\n             String manipulation")
+# print("String Concatenation is done with the +" + " sign")
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with backslash and the letter n.\n")
+# print("Trying a new change to the init commit, on day 1")
+# print("Working on staged changes.")
 
-print("Hello " + input("What is your name?: "))
 
-print("The number of strings in your name is ", len(input("What is your name? ")))
+# input("What is your name?  ")
 
-a = input("a: ")
-b = input("b: ")
-temp = a
-a = b
-b = temp
-print("a: ", a)
-print("b: ", b)
+# print("Hello " + input("What is your name?: "))
+
+# print("The number of strings in your name is ", len(input("What is your name? ")))
+
+# a = input("a: ")
+# b = input("b: ")
+# temp = a
+# a = b
+# b = temp
+# print("a: ", a)
+# print("b: ", b)
+
+print("         Welcome to Band name Generator")
+city_name = input("What city did you grow up?\n")
+pet_name = input("What is the name of your favorite pet?\n")
+print("The name of your new band is: ", city_name, pet_name +'s')
