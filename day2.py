f51a08d122798471cb5ce9b864b475016112de8b
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-09-06 02:05:57 +0300

    Add files via upload

--- day2.py
+++ day2.py
@@ -16,3 +16,22 @@ float_num = float(num)
 print(type(float_num))
 bool_num = bool(num)
 print(type(bool_num))
+
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+if len(two_digit_number)  != 2:
+    print("Input not 2 digits")
+else:
+    string_2_digit_number = str(two_digit_number)
+    print("First digit = ",string_2_digit_number[0])
+    print("Second digit = ",string_2_digit_number[1])
+    first_digit = string_2_digit_number[0]
+    second_digit = string_2_digit_number[1]
+    int_first_digit = int(first_digit)
+    int_second_digit = int(second_digit)
+    print("Sum of",int_first_digit, "and" ,int_second_digit, "=",int_first_digit + int_second_digit)
