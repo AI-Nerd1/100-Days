711e498d220537ca81802810cf160aff17a1b1be
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-09-06 01:37:07 +0300

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,18 @@
+print("\n                 ==========Day 2==========")
+print("\n                 ==========Data Types==========")
+print("Hello"[4])
+print(1_234)
+name = 3.1415
+print(type(name))
+
+new_name = str(name)
+print(len(new_name))
+
+num = 1234
+print(type(num))
+str_num = str(num)
+print(type(str_num))
+float_num = float(num)
+print(type(float_num))
+bool_num = bool(num)
+print(type(bool_num))
