84a22574f4e62d1c49c683ea36139b5326d65b09
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-09-06 23:16:43 +0300

    Add files via upload

--- day3.py
+++ day3.py
@@ -117,3 +117,38 @@ if(pizza_size == "L" or pizza_size == "l"):
         bill += 1
 print(f"Your bill is ${round(bill)}.00\n     Thank you for patronizing us")
     
+
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+print("\n           ==========Love Calculator==========\n")
+
+name1 = input("Ladies first, What is your name dear: ")
+name2 = input("Please enter your name Gentleman: ")
+
+name1 = name1.lower()
+name2 = name2.lower()
+
+true_count1 = name1.count('t')+ name1.count('r') +name1.count('u')+ name1.count('e')
+true_count2 = name2.count('t')+ name2.count('r') +name2.count('u')+ name2.count('e')
+
+love_count1 = name1.count('l')+ name1.count('o') +name1.count('v')+ name1.count('e')
+love_count2 = name2.count('l')+ name2.count('o') +name2.count('v')+ name2.count('e')
+
+true_count = true_count1 + true_count2
+love_count = love_count1 + love_count2
+
+love_score = str(true_count)+str(love_count)
+love_score = int(love_score)
+
+if love_score < 10 or love_score > 90:
+    print(f"Your score is {love_score}, you go together like coke and mentos.")
+
+elif love_score > 39 or love_score < 51:
+    print(f"Your score is {love_score}, you are alright together.")
+else:
+    print(f"Your score is {love_score}.")
