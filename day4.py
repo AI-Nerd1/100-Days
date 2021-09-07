47566e6cb1e58a21e4a4f835a0adc6077022d5e8
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-09-08 01:36:41 +0300

    Add files via upload

--- day4.py
+++ day4.py
@@ -51,6 +51,7 @@ while 1>0:
     elif computer_choice == user_choice:
         print("It's a draw")
 
+print("\n\n           ==========Random integer==========")
 import random
 
 random_integer = random.randint(0,20)
@@ -62,6 +63,7 @@ print(f"\nRandom float = {random_float}\n")
 random_large_float = random_integer * random_float
 print(f"\nRandom mixed number = {random_large_float}\n")
 
+print("\n\n           ==========Virtual Coin==========")
 
 face = random.randint(0,1)
 #?print(face)
@@ -69,3 +71,35 @@ if face == 0:
     print("Tails")
 else:
     print("Heads")
+import random
+print("\n\n           ==========Who's paying==========")
+names = ["Logan", "Adele", "Jevon", "Kruger", "Jason"]
+limit = len(names) -1
+payer = random.randint(0, limit)
+print(f"{names[payer]} is paying the bills today")
+
+print("\n\n           ==========Nested List==========")
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][1])
+print("\n\n           ==========Tresure map==========\n")
+#! 🚨 Don't change the code below 👇
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+#! 🚨 Don't change the code above 👆
+
+#? Write your code below this row 👇
+coordinate1 = int(position[0]) -1
+coordinate2 = int(position[1]) -1
+selected_row = map[coordinate1]
+selected_row [coordinate2] = "XX"
+#? Write your code above this row 👆
+#! 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
