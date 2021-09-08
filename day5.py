a0e964bca255c641f29493560e465b499b08a72c
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-09-09 00:17:26 +0300

    Add files via upload

--- day5.py
+++ day5.py
@@ -9,8 +9,38 @@ for x in days:
 print("\n                 ==========Height Average==========")
 height = [180, 124, 165, 173, 189, 169, 146]
 sum = 0
+count = 0
 for x in height:
-    sum = sum + x
+    sum += x
+    count += 1
 
-ave = sum / len(height)
-print(f"Average Height of {len(height)} students = {round(ave)}")
+ave = sum / count
+print(f"Average Height of {count} students = {round(ave)}")
+
+
+print("\n                 ==========Highest Score==========")
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+highest_score = 0
+for x in student_scores:
+    if x > highest_score:
+        highest_score = x
+
+print(f"The highest score in the class is: {highest_score}")
+
+
+
+print("                     ===========Even number Calculator===========")
+# for x in range(0, 10):
+#     print(x)
+even = 0
+for x in range(0, 101, 2):
+    even += x
+
+print(even)
