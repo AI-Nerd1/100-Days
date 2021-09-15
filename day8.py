47c5d83502fa291b1f26666d556afd46fc2a60b6
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-09-15 09:49:01 +0300

    Add files via upload

--- day8.py
+++ day8.py
@@ -96,19 +96,43 @@ def cipher():
     if direction == "decode":
         decrypt(alphabet, text, shift)
 
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
- 'u', 'v', 'w', 'x', 'y', 'z',' ' ,'a', 'b', 'c', 'd', 'e', 
+ 'u', 'v', 'w', 'x', 'y', 'z',
+ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+ '!', '#', '$', '%', '&', '(', ')', '*', '+',
+ ' ' ,'a', 'b', 'c', 'd', 'e', 
 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
-'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+'!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+def caeser(alphabet, text, shift, direction):
+    end_text = ""
+    if direction == "decode":
+        shift = shift * -1
+    for x in text:
+        position = alphabet.index(x)
+        new_position = position + shift
+        new_letter = alphabet[new_position]
+        end_text += new_letter
+    print(f"The {direction}d text is: {end_text}")
+
+
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-cipher()
+
+caeser(alphabet, text, shift, direction)
+
 next = input("To try again, tap Y, else tap N: ").lower()
-if next == "y":
-    cipher()
-
-
+check = True
+while check is True:
+    if next == "y":
+        caeser(alphabet, text, shift, direction)
+    else:
+        check = False
 
