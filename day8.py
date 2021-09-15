452e027c02f01f076c27f013097fa6738151010e
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-09-15 10:09:30 +0300

    Add files via upload

--- day8.py
+++ day8.py
@@ -96,7 +96,25 @@ def cipher():
     if direction == "decode":
         decrypt(alphabet, text, shift)
 
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
 
+print(logo)
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
@@ -121,18 +139,16 @@ def caeser(alphabet, text, shift, direction):
     print(f"The {direction}d text is: {end_text}")
 
 
-
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-caeser(alphabet, text, shift, direction)
-
-next = input("To try again, tap Y, else tap N: ").lower()
 check = True
 while check is True:
-    if next == "y":
-        caeser(alphabet, text, shift, direction)
-    else:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    if shift > len(alphabet):
+        shift = shift % len(alphabet)
+    caeser(alphabet, text, shift, direction)
+    next = input("To try again, tap Y, else tap N: ").lower()
+    if next == "n":
         check = False
+        print("Encryption Ended")
 
