2fbb86646f69bd12394dc3468d94ff886c2fbaa4
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-09-16 23:53:33 +0300

    Add files via upload

--- day9.py
+++ day9.py
@@ -1,4 +1,24 @@
 # print("\n                 ==========Day 9==========")
 # print("\n           ==========Python Dictionaries==========")
 
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
 
+
+empty_dictionary = {}
+print(empty_dictionary)
+empty_dictionary["First define"] = "Initial defination of the contents of a library"
+empty_dictionary["Second define"] = "Second definition of the contents of a library"
+
+print(empty_dictionary["Second define"])
