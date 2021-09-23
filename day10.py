5f719f0a8d40d83834eca735bd5ed2c2e4348195
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-09-23 10:47:56 +0300

    Add files via upload

--- day10.py
+++ day10.py
@@ -50,6 +50,7 @@ def format_name(f_name, l_name):
   return f"Result: {formated_f_name} {formated_l_name}"
 
 def is_leap(year):
+  """For determining if a year has over 365 days."""
   if year % 4 == 0:
     if year % 100 == 0:
       if year % 400 == 0:
@@ -62,12 +63,13 @@ def is_leap(year):
     return False
 
 def days_in_month(year, month):
+    """Passed to know how many days are in a month.
+     Value for February is the number of days cahnges if is_leap(year) returns True """
     month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]  
     if is_leap(year) is True:
       month_days[1] = 29
-    month = month - 1
-    x = month_days[month]
-    return x
+    return month_days[month - 1]
+    
 
   
   
@@ -76,3 +78,71 @@ year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
 print(days)
+
+
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+
+print(logo)
+
+
+#Calculator
+# from replit import clear
+# from art import logo
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  print(logo)
+
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      # clear()
+      calculator()
+
+calculator()
