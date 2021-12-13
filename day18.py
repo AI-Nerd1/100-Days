569b64860b2834765c8f5bf74cc51cfb2e4d4527
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-12-13 23:16:38 +0300

    Add files via upload

--- day18.py
+++ day18.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from turtle import Turtle, Screen
+import random
 
 cursor = Turtle()
 
@@ -38,80 +39,94 @@ cursor.ht()
 # bot.pu()
 # bot.home()
 
-def traces():
-    for x in range(10):
-        bot.fd(5)
-        bot.pu()
-        bot.fd(5)
-        bot.pd()
+# def traces():
+#     for x in range(10):
+#         bot.fd(5)
+#         bot.pu()
+#         bot.fd(5)
+#         bot.pd()
 
-def triangle():
-    bot.pencolor("red")
-    for x in range(3):
-        # bot.fd(50)
-        traces()
-        bot.rt(120)
-    bot.home()
+# def triangle():
+#     bot.pencolor("red")
+#     for x in range(3):
+#         # bot.fd(50)
+#         traces()
+#         bot.rt(120)
+#     bot.home()
 
-def square():
-    bot.pencolor("blue")
-    for x in range(4):
-        traces()
-        bot.rt(90)
-    bot.home()
+# def square():
+#     bot.pencolor("blue")
+#     for x in range(4):
+#         traces()
+#         bot.rt(90)
+#     bot.home()
 
-def pentagon():
-    bot.pencolor("yellow")
-    for x in range(5):
-        traces()
-        bot.rt(72)
-    bot.home()
+# def pentagon():
+#     bot.pencolor("yellow")
+#     for x in range(5):
+#         traces()
+#         bot.rt(72)
+#     bot.home()
 
-def hexagon():
-    bot.pencolor("green")
-    for x in range(6):
-        traces()
-        bot.rt(60)
-    bot.home()
+# def hexagon():
+#     bot.pencolor("green")
+#     for x in range(6):
+#         traces()
+#         bot.rt(60)
+#     bot.home()
 
-def heptagon():
-    bot.pencolor("orange")
-    for x in range(7):
-        traces()
-        bot.rt(51.5)
-    bot.home()
+# def heptagon():
+#     bot.pencolor("orange")
+#     for x in range(7):
+#         traces()
+#         bot.rt(51.5)
+#     bot.home()
 
-def octagon():
-    bot.pencolor("brown")
-    for x in range(8):
-        traces()
-        bot.rt(45)
-    bot.home()
+# def octagon():
+#     bot.pencolor("brown")
+#     for x in range(8):
+#         traces()
+#         bot.rt(45)
+#     bot.home()
 
-def nonagon():
-    bot.pencolor("grey")
-    for x in range(9):
-        traces()
-        bot.rt(40)
-    bot.home()
+# def nonagon():
+#     bot.pencolor("grey")
+#     for x in range(9):
+#         traces()
+#         bot.rt(40)
+#     bot.home()
 
-def decagon():
-    bot.pencolor("pink")
-    for x in range(10):
-        traces()
-        bot.rt(36)
-    bot.home()
+# def decagon():
+#     bot.pencolor("pink")
+#     for x in range(10):
+#         traces()
+#         bot.rt(36)
+#     bot.home()
 
 
-triangle()
-square()
-pentagon()
-hexagon()
-heptagon()
-octagon()
-nonagon()
-decagon()
+# triangle()
+# square()
+# pentagon()
+# hexagon()
+# heptagon()
+# octagon()
+# nonagon()
+# decagon()
 
+screen.screensize(2000,1500)
+angles = [90,180,0,270]
+bot.speed(0)
+
+colors = ["darkblue", "DarkOrange","LawnGreen", "MediumVioletRed",
+ "Salmon", "DeepSkyBlue", "Red", "Yellow", "green",
+  "orange", "indigo","Blue"]
+
+for x in range(1000):
+    bot.pencolor(random.choice(colors))
+    bot.rt(random.choice(angles))
+    bot.fd(random.randint(20,30))
+    bot.lt(random.choice(angles))
+    bot.fd(random.randint(20,30))
 
 
 screen.exitonclick()
