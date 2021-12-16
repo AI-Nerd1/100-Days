aba61fb66afc4dcbf2de486ca07a2ff2616b1efd
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-12-16 12:38:06 +0300

    Add files via upload

--- snakegame.py
+++ snakegame.py
@@ -1,14 +1,36 @@
 #!/usr/bun/env python3
 import turtle
 import random
+import time
 from turtle import Turtle, Screen
+from snake import Snake
 
 screen = Screen()
 screen.title("SNAKE GAME")
 screen.setup(height = 600, width = 600)
 screen.bgcolor("black")
-snake = Turtle()
+screen.tracer(0)
+snake = Snake()
 
 
-screen.listen()
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.08)
+
+    snake.move()
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
+    
+
+    
+    
+        
+
+
+
+
 screen.exitonclick()
