61a10e408e3efa85f84187820f48b24cb6cb304e
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-12-17 23:44:18 +0300

    Add files via upload

--- snakegame.py
+++ snakegame.py
@@ -4,6 +4,7 @@ import random
 import time
 from turtle import Turtle, Screen
 from snake import Snake
+from food import Food
 
 screen = Screen()
 screen.title("SNAKE GAME")
@@ -11,6 +12,7 @@ screen.setup(height = 600, width = 600)
 screen.bgcolor("black")
 screen.tracer(0)
 snake = Snake()
+food = Food()
 
 
 game_is_on = True
@@ -19,11 +21,16 @@ while game_is_on:
     time.sleep(0.08)
 
     snake.move()
+    if snake.head.distance(food) < 15:
+        print("Eaten")
+        food.refresh()
+
+
     screen.listen()
-    screen.onkey(snake.up, "Up")
-    screen.onkey(snake.down, "Down")
-    screen.onkey(snake.left, "Left")
-    screen.onkey(snake.right, "Right")
+    screen.onkey(snake.up, "w")
+    screen.onkey(snake.down, "s")
+    screen.onkey(snake.left, "a")
+    screen.onkey(snake.right, "d")
     
 
     
