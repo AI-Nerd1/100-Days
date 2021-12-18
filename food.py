ad626271fe368d5407cc30f3726b7c8149ddc171
Adele <77436077+AI-Nerd1@users.noreply.github.com>
2021-12-19 01:48:36 +0300

    Add files via upload

--- food.py
+++ food.py
@@ -1,12 +1,12 @@
 from turtle import Turtle
 import random
+direction = [0,45, 90, 135, 180, 225, 270, 315]
 class Food(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape("circle")
+        self.shape("turtle")
         self.penup()
-        self.shapesize(stretch_len = 0.3, stretch_wid = 0.3)       
-        self.color("orange")
+        self.shapesize(stretch_len = 0.6, stretch_wid = 0.6)  
         self.speed("fastest")
         self.refresh()
 
@@ -14,3 +14,8 @@ class Food(Turtle):
         rand_x = random.randint(-280, 280)
         rand_y = random.randint(-280, 280)
         self.goto(rand_x, rand_y)
+        colors = ["darkblue", "DarkOrange","LawnGreen", "MediumVioletRed",
+"Salmon", "DeepSkyBlue", "Red", "Yellow", "green",
+"orange", "indigo","Blue"]     
+        self.color(random.choice(colors))
+        self.setheading(random.randint(0,360))
