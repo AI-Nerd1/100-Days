7864ab62c1c89ab73d6b4c84cf63f621a7063774
Adele Chinda <chindahadele2@gmail.com>
2022-05-28 19:26:15 +0300

    Up to main

--- Magoosh Flashcard App/flashcard.py
+++ Magoosh Flashcard App/flashcard.py
@@ -26,12 +26,12 @@ def next_card():
 
     
 def flip_card():
-    
+    global current_card
     canvas.grid(row=0, column=0)
-    card_word = canvas.create_text(400,230,text = "", font = ("garamond", 40, "bold"))
+    
     #* Front button
    
-   # canvas.itemconfig(card_word, text = current_card["WORD"])
+    canvas.itemconfig(card_word1, text = current_card["WORD"])
     canvas.itemconfig(card_background, image = card_front_image)
     
 
@@ -56,6 +56,7 @@ canvas.grid(row=0, column=0, columnspan =2)
 
     #* Should be backend
 card_word = canvas.create_text(400,100,text = "word", font = ("garamond", 40))
+card_word1 = canvas.create_text(400,230,text = "", font = ("garamond", 40, "bold"))
 card_meaning = canvas.create_text(400,250,text = "meaning", font = ("Ariel", 18, "italic"), justify= "left")
 
       #* back buttons
@@ -68,10 +69,10 @@ wrong_button = Button(image = wrong_image)
 flip_button = Button(image = flip_image)
 
 right_button.grid(row=1, column=1)
-right_button.config(bg = BACKGROUND_COLOR,border=0, highlightthickness=0, command= next_card)
+right_button.config(bg = BACKGROUND_COLOR,border=0, highlightthickness=0, command= flip_card)
 
 wrong_button.grid(row=1, column=0)
-wrong_button.config(bg = BACKGROUND_COLOR,border=0, highlightthickness=0, command = next_card)
+wrong_button.config(bg = BACKGROUND_COLOR,border=0, highlightthickness=0, command = flip_card)
 
 flip_button.grid(row=0, column=3)
 flip_button.config(bg = BACKGROUND_COLOR,border=0, highlightthickness=0, command = next_card)
