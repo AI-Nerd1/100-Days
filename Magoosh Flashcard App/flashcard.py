092848e31d134030d536345caa6ec580361cc14c
Adele Chinda <chindahadele2@gmail.com>
2022-05-28 17:18:26 +0300

    frontend concluded

--- Magoosh Flashcard App/flashcard.py
+++ Magoosh Flashcard App/flashcard.py
@@ -35,27 +35,28 @@ flip_button.config(bg = BACKGROUND_COLOR,border=0, highlightthickness=0)
 
 
 
-back_canvas = Canvas(width = 800, height = 500)
+#//back_canvas = Canvas(width = 800, height = 500)
                #? card back
 card_back_image = PhotoImage(file ="/home/logan/github/100-Days/Magoosh Flashcard App/images/card_back.png")
-back_canvas.create_image(400, 263, image = card_back_image)
-back_canvas.config(bg = BACKGROUND_COLOR, highlightthickness=0)
-back_canvas.grid(row=0, column=0, columnspan = 2)
+front_canvas.create_image(400, 263, image = card_back_image)
+front_canvas.config(bg = BACKGROUND_COLOR, highlightthickness=0)
+front_canvas.grid(row=0, column=0, columnspan = 2)
 
     #* Should be backend
-back_canvas.create_text(400,50,text = "word", font = ("garamond", 40))
-back_canvas.create_text(100,150,text = "meaning", font = ("Ariel", 14, "italic"))
+front_canvas.create_text(400,50,text = "word", font = ("garamond", 40))
+front_canvas.create_text(100,150,text = "meaning", font = ("Ariel", 14, "italic"))
 
       #* back buttons
 right_image = PhotoImage(file ="/home/logan/github/100-Days/Magoosh Flashcard App/images/right.png")
 wrong_image = PhotoImage(file ="/home/logan/github/100-Days/Magoosh Flashcard App/images/wrong.png")
+
 right_button = Button(image = right_image)
 wrong_button = Button(image = wrong_image)
 
-right_button.grid(row=1, column=0)
+right_button.grid(row=1, column=1)
 right_button.config(bg = BACKGROUND_COLOR,border=0, highlightthickness=0)
 
-wrong_button.grid(row=1, column=2)
+wrong_button.grid(row=1, column=0)
 wrong_button.config(bg = BACKGROUND_COLOR,border=0, highlightthickness=0)
 
 
