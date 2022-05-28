6b0eacb759f039241e912969c29f203d49551054
Adele Chinda <chindahadele2@gmail.com>
2022-05-28 15:45:35 +0300

    Added card front and back

--- Magoosh Flashcard App/flashcard.py
+++ Magoosh Flashcard App/flashcard.py
@@ -5,6 +5,7 @@
 #TODO read_file.to_csv (r'/home/logan/github/100-Days/Magoosh Flashcard App/wordlist.csv', index = None, header=True)
 
 from tkinter import *
+from turtle import bgcolor
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -12,15 +13,29 @@ window = Tk()
 window.title("Flashcards")
 window.config(padx = 50, pady = 50, bg = BACKGROUND_COLOR)
 
-canvas = Canvas(width = 800, height = 500)
-card_front_image = PhotoImage(file ="/home/logan/github/100-Days/Magoosh Flashcard App/images/card_front.png")
-canvas.create_image(400, 263, image = card_front_image)
-canvas.grid(row=0, column=0)
-window.mainloop()
-
 """
     Front End Modifications here
 """
+canvas = Canvas(width = 800, height = 500)
+        #? card front
+card_front_image = PhotoImage(file ="/home/logan/github/100-Days/Magoosh Flashcard App/images/card_front.png")
+canvas.create_image(400, 263, image = card_front_image)
+#* Should be backend
+canvas.create_text(400,150,text = "word", font = ("Ariel", 40))
+#canvas.create_text(400,250,text = "meaning", font = ("liberation", 14, "italic"))
+
+
+        #? card back
+card_back_image = PhotoImage(file ="/home/logan/github/100-Days/Magoosh Flashcard App/images/card_back.png")
+canvas.create_image(400, 263, image = card_back_image)
+
+
+
+canvas.config(bg = BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0)
+window.mainloop()
+
+
 
 
 """
