1595097653b001e2cb2215c67abf70c7f3c4c114
Adele Chinda <chindahadele2@gmail.com>
2022-05-28 13:57:27 +0300

    tkinter mod

--- Magoosh Flashcard App/flashcard.py
+++ Magoosh Flashcard App/flashcard.py
@@ -4,3 +4,14 @@
 #TODO read_file = pd.read_excel (r'/home/logan/github/100-Days/Magoosh Flashcard App/wordlist.xlsx')
 #TODO read_file.to_csv (r'/home/logan/github/100-Days/Magoosh Flashcard App/wordlist.csv', index = None, header=True)
 
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flashcards")
+window.config(padx = 50, pady = 50, bg = BACKGROUND_COLOR)
+
+canvas = Canvas(width = 800, height = 500)
+window.mainloop()
+
