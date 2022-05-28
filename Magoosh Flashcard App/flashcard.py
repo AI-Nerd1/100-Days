02a97dcb14d37b96a19e3cc37da740c6f46de0dc
Adele Chinda <chindahadele2@gmail.com>
2022-05-28 13:45:26 +0300

    xlsx to csv

--- Magoosh Flashcard App/flashcard.py
+++ Magoosh Flashcard App/flashcard.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
-from game_data import data
-import random
+"""Convert xlsx to csv"""
+#! import pandas as pd
+#TODO read_file = pd.read_excel (r'/home/logan/github/100-Days/Magoosh Flashcard App/wordlist.xlsx')
+#TODO read_file.to_csv (r'/home/logan/github/100-Days/Magoosh Flashcard App/wordlist.csv', index = None, header=True)
+
