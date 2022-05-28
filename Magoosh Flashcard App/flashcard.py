ceb251b0ec99ed0fa33cc19b8439209e2d3da6cb
Adele Chinda <chindahadele2@gmail.com>
2022-05-28 19:35:25 +0300

    main trial

---
+++ Magoosh Flashcard App/flashcard.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Convert xlsx to csv"""
+#! import pandas as pd
+#TODO read_file = pd.read_excel (r'/home/logan/github/100-Days/Magoosh Flashcard App/wordlist.xlsx')
+#TODO read_file.to_csv (r'/home/logan/github/100-Days/Magoosh Flashcard App/wordlist.csv', index = None, header=True)
+
+from tkinter import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flashcards")
+window.config(padx = 50, pady = 50, bg = BACKGROUND_COLOR)
+
+canvas = Canvas(width = 800, height = 500)
+card_front_image = PhotoImage(file ="/home/logan/github/100-Days/Magoosh Flashcard App/images/card_front.png")
+canvas.create_image(400, 263, image = card_front_image)
+canvas.grid(row=0, column=0)
+
+
+"""
+    Front End Modifications here
+"""
+
+
+
+
+"""
+    Back End Modifications here
+"""
+
+
+
+
+window.mainloop()
