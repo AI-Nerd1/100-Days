d05162e3247e32667deebba7d5869b94ba63bbe3
Adele Chinda <chindahadele2@gmail.com>
2022-05-29 17:44:00 +0300

    Recursive iteration of card_word to change

--- Magoosh Flashcard App/tempCodeRunnerFile.py
+++ Magoosh Flashcard App/tempCodeRunnerFile.py
@@ -1,3 +1,2 @@
-card_word = canvas.create_text(400,230,text = "", font = ("garamond", 40, "bold"))
-# card_word1 = canvas.create_text(400,150,text = "", font = ("garamond", 25, "bold"), justify= "center")
-# card_meaning = canvas.create_text(400,250,text = "meaning", font = ("Ariel
+card_word1 = canvas.create_text(400,150,text = "", font = ("garamond", 25, "bold"), justify= "center")
+    # card_meaning = canvas.create_text(400,250,text = "meaning", font = ("Ariel", 18, "
